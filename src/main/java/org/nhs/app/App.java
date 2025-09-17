fa13ab1c34497d1b749ed258bc4eb06a392c9c97
rhythmsaini333 <2022964@apps.nsd.org>
2025-09-17 15:42:17 +0000

    Done! :D

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,6 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-package org.nhs.app;
-
-// Your code goes here
+ class App 
+{
+    public static void main(String[] args){
+        System.out.print("hello world!");
+    }
+}
